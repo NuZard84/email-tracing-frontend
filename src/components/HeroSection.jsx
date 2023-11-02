61c47b361be6d98c99792475121e066d9c0ab6a3
NuZrd84 <86848690+NuZard84@users.noreply.github.com>
2023-11-02 23:18:44 +0530

    almost complete herosection

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,4 +1,5 @@
-import { Box, Heading, Image, Text } from "@chakra-ui/react";
+import { Box, Button, Heading, Image, Text } from "@chakra-ui/react";
+import { fonts } from "../utils/colors/colors";
 
 const HeroSection = () => {
   return (
@@ -6,17 +7,47 @@ const HeroSection = () => {
       display={"flex"}
       justifyContent={"center"}
       alignItems={"center"}
-      flexDir={"col"}
+      flexDir={"column"}
     >
-      <Box display={"flex"} justifyContent={"center"} alignItems={"center"}>
-        <Box min={"75%"}>
-          <Heading> Email Tracking Made Easy</Heading>
-          <Text>
+      <Box
+        display={"flex"}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+        width={"full"}
+      >
+        <Box display={"flex"} flexDir={"column"} width={"50%"} gap={4}>
+          <Heading
+            fontSize={"70px"}
+            lineHeight={1.1}
+            textColor={fonts.primaryColor}
+          >
+            Email Tracking Made Easy
+          </Heading>
+          <Text fontSize={"lg"} textColor={fonts.primaryColor}>
             Track emails and extract valuable details from headers with our
             powerful email tracking web app.
           </Text>
+          <Button
+            transition={"all 0.2s ease-in-out"}
+            transitionDuration={"300ms"}
+            _hover={{
+              backgroundColor: fonts.tertiaryColor,
+              textColor: fonts.primaryColor,
+              border: "1px solid",
+              boxShadow: "xl",
+            }}
+            _active={{
+              transform: "translateY(5px)",
+            }}
+            width={"40%"}
+            backgroundColor={fonts.primaryColor}
+            textColor={fonts.secondaryColor}
+            padding={4}
+          >
+            Get Start
+          </Button>
         </Box>
-        <Image src={import.meta.env.VITE_MAIL_IMG_LINK} boxSize={"400px"} />
+        <Image src={import.meta.env.VITE_MAIL_IMG_LINK} boxSize={"600px"} />
       </Box>
     </Box>
   );
