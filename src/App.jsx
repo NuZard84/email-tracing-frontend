17ef0e52ebda2bbed91782c942189d2a362914a5
NuZrd84 <86848690+NuZard84@users.noreply.github.com>
2023-11-02 21:53:20 +0530

    comps added

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { Box, Image } from "@chakra-ui/react";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return (
-    <Box height={"95vh"}>
-      <h1>The end of fucking world.</h1>
-      <div>
-        hello this is email tracing app and you can trace the email by headrers
-      </div>
-      <div>
-        <h1>hello</h1>
-        <Image src={import.meta.env.VITE_MAIL_IMG_LINK} boxSize={"500px"} />
-      </div>
-    </Box>
-  );
+  return <Box>hello</Box>;
 }
 
 export default App;
