570ee927b29422896861a49d4f73a42cfef684e3
NuZrd84 <86848690+NuZard84@users.noreply.github.com>
2023-11-02 18:18:19 +0530

    set background

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,17 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { Box } from "@chakra-ui/react";
-import Background from "./assets/background";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <Box height={"95vh"}>
+      <h1>The end of fucking world.</h1>
       <div>
         hello this is email tracing app and you can trace the email by headrers
       </div>
       <div>
-        <Background />
         <h1>hello</h1>
       </div>
     </Box>
