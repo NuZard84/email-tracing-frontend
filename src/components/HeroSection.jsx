9823ff9c8c192efbd5302464575628c5cffd2516
NuZrd84 <86848690+NuZard84@users.noreply.github.com>
2023-11-02 22:31:31 +0530

    changes

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,10 +1,22 @@
-import { Box, Image } from "@chakra-ui/react";
+import { Box, Heading, Image, Text } from "@chakra-ui/react";
 
 const HeroSection = () => {
   return (
-    <Box>
-      <Box>
-        <Image />
+    <Box
+      display={"flex"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      flexDir={"col"}
+    >
+      <Box display={"flex"} justifyContent={"center"} alignItems={"center"}>
+        <Box min={"75%"}>
+          <Heading> Email Tracking Made Easy</Heading>
+          <Text>
+            Track emails and extract valuable details from headers with our
+            powerful email tracking web app.
+          </Text>
+        </Box>
+        <Image src={import.meta.env.VITE_MAIL_IMG_LINK} boxSize={"400px"} />
       </Box>
     </Box>
   );
