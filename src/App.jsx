01ffad233beddccfbea20bc4f9da2f0d4eafc4b2
NuZrd84 <86848690+NuZard84@users.noreply.github.com>
2023-11-02 21:50:07 +0530

    initalized

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { Box } from "@chakra-ui/react";
+import { Box, Image } from "@chakra-ui/react";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -15,6 +15,7 @@ function App() {
       </div>
       <div>
         <h1>hello</h1>
+        <Image src={import.meta.env.VITE_MAIL_IMG_LINK} boxSize={"500px"} />
       </div>
     </Box>
   );
